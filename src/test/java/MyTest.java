c0260b822e243b9e8b17ec05c0876d6a70afe330
Divyapatel27 <83128168+Divyapatel27@users.noreply.github.com>
2021-05-02 00:47:49 +0530

    Update MyTest.java

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -1,11 +1,55 @@
 import com.microsoft.demo.Demo;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class MyTest {
+	public static WebDriver driver;
     @Test
-    public void test_method_1() {
-        Demo d = new Demo();
-        d.DoSomething(true);
+    public void test_method_1() throws InterruptedException {
+    	By textbox_firstname=By.id("usernamereg-firstName");
+    	By textbox_lastname=By.id("usernamereg-lastName");
+    	By textbox_username=By.id("usernamereg-yid");
+    	By textbox_password=By.id("usernamereg-password");
+    	By textbox_mobileno=By.id("usernamereg-phone");
+    	By select_dob_month=By.id("usernamereg-month");
+    	By textbox_dob_day=By.id("usernamereg-day");
+    	By textbox_dob_year=By.id("usernamereg-year");
+    	By continuebtn=By.id("reg-submit-button");
+    	System.setProperty("webdriver.chrome.driver","C:\\chromedriver.exe");
+    	driver=new ChromeDriver();
+    	
+    	driver.navigate().to("https://login.yahoo.com/account/create");
+    	
+    	driver.findElement(textbox_firstname).sendKeys("ABC");
+    	Thread.sleep(5000);
+    	driver.findElement(textbox_lastname).sendKeys("BHHJ");
+    	Thread.sleep(5000);
+    	driver.findElement(textbox_username).sendKeys("jhjbhkjsdkjnfksjbk");
+    	Thread.sleep(5000);
+    	driver.findElement(textbox_username).sendKeys(Keys.TAB);
+    	//driver.findElement(username).click();
+    	Thread.sleep(5000);
+    	driver.findElement(textbox_password).sendKeys("Testing@1");
+    	Thread.sleep(5000);
+    	driver.findElement(textbox_mobileno).sendKeys("9877665540");
+    	Thread.sleep(5000);
+    	Select select=new Select(driver.findElement(select_dob_month));
+    	select.selectByIndex(1);
+    	Thread.sleep(5000);
+    	driver.findElement(textbox_dob_day).sendKeys("12");
+    	Thread.sleep(5000);
+    	driver.findElement(textbox_dob_year).sendKeys("1990");
+    	Thread.sleep(5000);
+    	//click on continue
+    	driver.findElement(continuebtn).click();
+    	
+    	driver.quit();
+    	
+    	
     }
 
     @Test
